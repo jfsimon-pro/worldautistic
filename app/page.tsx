e47a3774c970ce755cbf3f03f01c908cf620a70a
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2025-12-22 09:46:55 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from '../styles/Login.module.css';
+import styles from './styles/Login.module.css';
 
 export default function SignInPage() {
   return (
