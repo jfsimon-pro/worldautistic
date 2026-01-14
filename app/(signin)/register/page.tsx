333591417ebff9c4a76079b6824dc3a529dbc988
Simon <77901095+jfsimon-pro@users.noreply.github.com>
2026-01-14 02:00:42 -0300

    fix: register back button now returns to main page

--- app/(signin)/register/page.tsx
+++ app/(signin)/register/page.tsx
@@ -91,7 +91,7 @@ export default function RegisterPage() {
       <div className={styles['header-container']}>
 
         {/* Back Button */}
-        <Link href="/(signin)" className={styles['header-btn']}>
+        <Link href="/" className={styles['header-btn']}>
           <svg xmlns="http://www.w3.org/2000/svg" className={styles['header-icon']} fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
           </svg>
